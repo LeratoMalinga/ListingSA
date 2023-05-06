7abae7094e63f6cc0c7393da29a4a4d02bd218a9
Lerato Malinga <lmalinga@retrorabbit.co.za>
2023-05-06 14:22:57 +0200

    logoutfeature

--- Personal Project Svelte/ListingSA/src/routes/+page.svelte
+++ Personal Project Svelte/ListingSA/src/routes/+page.svelte
@@ -26,18 +26,6 @@
 </script>
 <Toaster/>
 
-<header>
-    <nav>
-        <div class="logoImage">
-            <img src="/Logohome2.png" alt ="logo" width="90px" height="90px">
-        </div>
-        <div class="links">
-            <a href="/">Home</a>
-            <a href="/">About</a>
-            <a href="/login">Login</a>
-        </div>
-    </nav>   
-</header>
 
 <body>
 
@@ -109,53 +97,11 @@
     position: relative;
     left:25%;
    }
-   /*.tellphoneinformatincontainer{
-    position: relative;
-    left: 90px;
-    top:0px;
-   }*/
-   
-
-   
-
-header {
-        height: 90px;
-        text-align: center;
-        background:rgb(96.1%, 96.1%, 96.1%);
-        padding-left: 0%;
-    
-    
-    }
-    .links{
-        margin-left:auto;
-        margin-top: 0px;
-        margin-right: 0%;
-        
-    }
-    a {
-        margin-left: 40px;
-        margin-bottom: 20px;
-        color: black;
-    
-        
-    }
-
-    .logoImage{
-        margin-left: 2%;
-    }
-
-    nav {
-        display:flex;
-        align-items:center;
-        max-width: 90%;
-        margin :0 auto;
-        margin-left:0;
-    }
-    img{
+img{
         margin-left: 0%;
     }
 
-    
+
      body{
         background-image:  url(/background.jpg);
         background-repeat: no-repeat;
