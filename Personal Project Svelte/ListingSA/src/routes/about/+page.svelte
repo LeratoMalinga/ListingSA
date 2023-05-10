9f99a0ab6574fbcbcda233547252f68eb436f1fa
Lerato Malinga <lmalinga@retrorabbit.co.za>
2023-05-10 15:59:32 +0200

    about us page updates

--- Personal Project Svelte/ListingSA/src/routes/about/+page.svelte
+++ Personal Project Svelte/ListingSA/src/routes/about/+page.svelte
@@ -2,12 +2,21 @@
 <div class="container">
     <div class="infocontainer">
     <p style="font-weight:bold; font-size:20px; margin-left:39%;">About us</p>
-    <p style="font-weight:bold; font-size:20px;">Listing SA</p>
+    <p style="font-weight:bold; font-size:20px; color:green;">Listing SA</p>
     <p>Welcome to Your Listing Website! We are dedicated to providing you with the best platform for finding and listing various items.</p>
       <p>Our mission is to connect Agents and Tenants in a convenient and user-friendly manner, offering a wide range of categories and listings to suit your needs.</p>
     </div>  
-   
 
+    <div>
+        <h3 style="color:green;">Contact Us</h3>
+      <p>If you have any questions, feedback, or suggestions, we would love to hear from you. Please feel free to reach out to us using the contact information below:</p>
+      <ul>
+        <li style="color:white;">Email: info@yourlistingwebsite.com</li>
+        <li style="color:white;">Phone: 123-456-7890</li>
+        <li style="color:white;">Address: 123 Main Street, City, Country</li>
+      </ul>
+    </div>
+   
     <div class="icons">
         <img src="/Twitter - Negative.png" alt="" width="20px"> 
         <img src="/Instagram - Negative.png" alt="" width="20px"> 
@@ -22,7 +31,7 @@
   background:rgb(44,60,91);
   max-width: 700px;
   width: 90%;
-  height: 50svh;
+  height: 60vh;
   border-radius: 15px;
   margin: 15% auto 0;
   padding: 20px;
@@ -41,6 +50,6 @@
   margin: 0 auto;
   max-width: 400px;
   margin-left: 25%;
-  margin-top: 20%;
+  margin-top: 10%;
 }
 </style>
