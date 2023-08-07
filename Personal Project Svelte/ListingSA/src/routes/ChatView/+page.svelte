c76bb820564e500da0142b83a4b9f5b93a76a95f
Lerato Malinga <lmalinga@retrorabbit.co.za>
2023-08-07 21:44:31 +0200

    chat view update

--- Personal Project Svelte/ListingSA/src/routes/ChatView/+page.svelte
+++ Personal Project Svelte/ListingSA/src/routes/ChatView/+page.svelte
@@ -8,19 +8,64 @@
  
 
   let chats : ChatMessage[] = [];
-  let receivers: string[] = [];
+  let receivers = [];
+  let message = '';
+  let messages = []; 
+  let receivedMessage ='';
+  let receivedMessages = [];
   
+  let selectedReceiverId = null;
+
+
+  
+  const setSelectedReceiver = (receiverId) => {
+    selectedReceiverId = receiverId;
+    // Load chat history for the selected receiver
+    requestChatHistory(receiverId);
+  };
+
+  function isMessageSentByCurrentUser(messageUserId: string): boolean {
+    return messageUserId === userId;
+  }
+
+
   async function requestChatHistory(userId) {
     try {
       // Call the server-side method to request chat history for the given chat ID
       const chatHistory = await getChatHistory(userId);
+    
+      // Store unique users in the receivers array
+      receivers = Array.from(new Set(chatHistory.map(chat => ({
+        userId: chat.user.id,
+        fullName: chat.user.name,
+      }))));
+
       chats = chatHistory;
-      receivers = Array.from(new Set(chats.map(chat => chat.user.name)));
-      console.log(chats);
     } catch (err) {
       console.error('Error requesting chat history:', err);
     }
   }
+
+  const sendMessage = async () => {
+    if (!selectedReceiverId) {
+      console.error('No receiver selected.'); // Add proper error handling
+      return;
+    }
+
+    const chatMessage = {
+      User: userId, // User GUID or User ID as string
+      UserName: userName, // Optional, if applicable
+      Sender: userId, // Optional, if applicable
+      Receiver: selectedReceiverId, // Optional, if applicable
+      Message: message, // The actual message from the input field
+      // Current timestamp and Communication ID, if applicable
+    };
+
+    await signalRService.sendMessage(chatMessage);
+    messages = [...messages, { content: message, sender: true }];
+    message = '';
+  };
+  
 
 
   function getSenderUserEmail(): string {
@@ -70,12 +115,21 @@
   async function initializeSignalR() {
     await signalRService.initialize(userId, fullName, userName);
   }
+
+
   // Subscribe to the onReceiveMessage event in the signalR service
   onMount(() => {
     const userId =getSenderUserId();
     requestChatHistory(userId);
     initializeSignalR();
+
+    // Subscribe to the onReceiveMessage event in the signalR service
+  signalRService.onReceiveMessage((message) => {
+    // This callback will be called when a new message is received
+    receivedMessages = [...receivedMessages, message];
   });
+
+});
 
 
 </script>
@@ -84,6 +138,7 @@
   <BackButton />
 </div>
 
+<!-- ... Your existing code ... -->
 
 <div class="chat-container">
   <div class="sidebar">
@@ -91,62 +146,150 @@
     <div class="chat-list-container">
       <ul>
         {#each receivers as receiver}
-          <li>{receiver}</li>
-      {/each}
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <li class:selected={selectedReceiverId === receiver.userId} on:click={() => setSelectedReceiver(receiver.userId)}>
+            {receiver.fullName}
+          </li>
+        {/each}
       </ul>
     </div>
   </div>
+
+  <div class="chat-history">
+    <ul>
+      {#each chats as chat}
+        <li class:sender={isMessageSentByCurrentUser(chat.user.id)}>
+          {#if isMessageSentByCurrentUser(chat.user.id)}
+            You: {chat.message}
+          {:else}
+            {chat.user.name}: {chat.message}
+          {/if}
+        </li>
+      {/each}
+      {#each receivedMessages as message}
+        <li class:sender={!isMessageSentByCurrentUser(message.user.id)}>
+          {#if isMessageSentByCurrentUser(message.user.id)}
+            You: {message.message}
+          {:else}
+            {message.user.name}: {message.message}
+          {/if}
+        </li>
+      {/each}
+    </ul>
+
+    
+  <div class="message-input">
+    <input type="text" bind:value={message} />
+    <button on:click={sendMessage}>Send</button>
+  </div>
 </div>
+  </div>
+
+
+<!-- ... Your existing code ... -->
 
 <style>
-   
   .chat-container {
-   display: flex;
-   background-color: #f0f8ea; /* Light green background color */
-   padding: 16px;
-   border-radius: 8px;
-   max-width: 800px;
-   margin: 0 auto;
-   margin-top: 10px;
- }
+    display: flex;
+    background-color: #f0f8ea; /* Light green background color */
+    padding: 16px;
+    border-radius: 8px;
+    max-width: 800px;
+    margin: 0 auto;
+    margin-top: 10px;
+  }
 
- .sidebar {
-   flex: 1;
-   background-color: #eaf8f0; /* Lighter green for the sidebar */
-   border-radius: 8px;
-   padding: 16px;
-   max-width: 200px;
- }
+  .sidebar {
+    flex: 1;
+    background-color: #eaf8f0; /* Lighter green for the sidebar */
+    border-radius: 8px;
+    padding: 16px;
+    max-width: 200px;
+  }
 
- .sidebar h3 {
-   margin-bottom: 16px;
-   font-size: 18px;
-   font-weight: bold;
- }
+  .sidebar h3 {
+    margin-bottom: 16px;
+    font-size: 18px;
+    font-weight: bold;
+  }
 
- .chat-list-container {
-   max-height: 300px; /* Adjust this to set the maximum height of the chat list container */
-   overflow-y: auto;
- }
+  .chat-list-container {
+    max-height: 300px; /* Adjust this to set the maximum height of the chat list container */
+    overflow-y: auto;
+  }
 
- .sidebar ul {
-   list-style: none;
-   padding: 0;
- }
+  .sidebar ul {
+    list-style: none;
+    padding: 0;
+  }
 
- .sidebar li {
-   padding: 8px;
-   cursor: pointer;
- }
+  .sidebar li {
+    padding: 8px;
+    cursor: pointer;
+  }
 
- .sidebar li:hover {
-   background-color: #d2f1d4; /* Slightly darker green on hover */
-   border-radius: 4px;
- }
+  .sidebar li:hover {
+    background-color: #d2f1d4; /* Slightly darker green on hover */
+    border-radius: 4px;
+  }
 
- .gobackbutton{
-       margin-top: 10%;
-       margin-left: 10%;
-   }
+  .gobackbutton {
+    margin-top: 10%;
+    margin-left: 10%;
+  }
+
+  .chat-history {
+    flex: 2;
+    padding: 16px;
+    background-color: #fff;
+    border-radius: 8px;
+    overflow-y: auto;
+    max-height: 500px;
+  }
+
+  .chat-history ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  .chat-history li {
+    margin-bottom: 8px;
+    padding: 8px;
+    border-radius: 4px;
+  }
+
+  .chat-history li.sender {
+    background-color: #aaf0d1; /* Light blue for messages sent by the current user */
+    align-self: flex-end;
+  }
+
+  .chat-history li:not(.sender) {
+    background-color: #f5f5f5; /* Light gray for messages received from others */
+  }
+
+  .message-input {
+    display: flex;
+    align-items: center;
+    margin-top: 16px;
+  }
+
+  .message-input input {
+    flex: 1;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  .message-input button {
+    margin-left: 8px;
+    padding: 8px 16px;
+    background-color: #4caf50; /* Green button color */
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  .message-input button:hover {
+    background-color: #45a049; /* Slightly darker green on hover */
+  }
 </style>
-
